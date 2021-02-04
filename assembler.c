7f9905e5ac3a4e104e30ee866b7c61ba4ba51a77
echen2048 <45611400+echen2048@users.noreply.github.com>
2021-02-03 20:25:03 -0600

    Add files via upload
    Initial Commit with provided code

---
+++ assembler.c
@@ -0,0 +1,118 @@
+#include <stdio.h> /* standard input/output library */
+#include <stdlib.h> /* Standard C Library */
+#include <string.h> /* String operations library */
+#include <ctype.h> /* Library for useful character operations */
+#include <limits.h> /* Library for definitions of common variable type characteristics */
+#include <stdbool.h>
+#define MAX_LINE_LENGTH 255
+enum{
+    DONE, OK, EMPTY_LINE
+};
+
+FILE* infile = NULL;
+FILE* outfile = NULL;
+
+int toNum(char * pStr);
+
+int main(int argc, char* argv[]) {
+    if(argc != 4){
+        printf("Insufficient Arguments");
+        return -1;
+    }
+     char *prgName   = NULL;
+     char *iFileName = NULL;
+     char *oFileName = NULL;
+
+     prgName   = argv[0];
+     iFileName = argv[1];
+     oFileName = argv[2];
+
+     printf("program name = '%s'\n", prgName);
+     printf("input file name = '%s'\n", iFileName);
+     printf("output file name = '%s'\n", oFileName);
+
+     /* open the source file */
+     infile = fopen(argv[1], "r");
+     outfile = fopen(argv[2], "w");
+		 
+     if (!infile) {
+       printf("Error: Cannot open file %s\n", argv[1]);
+       exit(4);
+		 }
+     if (!outfile) {
+       printf("Error: Cannot open file %s\n", argv[2]);
+       exit(4);
+     }
+
+     printf("test");
+
+     fclose(infile);
+     fclose(outfile);
+
+}
+
+int toNum( char * pStr )
+{
+   char * t_ptr;
+   char * orig_pStr;
+   int t_length,k;
+   int lNum, lNeg = 0;
+   long int lNumLong;
+
+   orig_pStr = pStr;
+   if( *pStr == '#' )				/* decimal */
+   { 
+     pStr++;
+     if( *pStr == '-' )				/* dec is negative */
+     {
+       lNeg = 1;
+       pStr++;
+     }
+     t_ptr = pStr;
+     t_length = strlen(t_ptr);
+     for(k=0;k < t_length;k++)
+     {
+       if (!isdigit(*t_ptr))
+       {
+	 printf("Error: invalid decimal operand, %s\n",orig_pStr);
+	 exit(4);
+       }
+       t_ptr++;
+     }
+     lNum = atoi(pStr);
+     if (lNeg)
+       lNum = -lNum;
+ 
+     return lNum;
+   }
+   else if( *pStr == 'x' )	/* hex     */
+   {
+     pStr++;
+     if( *pStr == '-' )				/* hex is negative */
+     {
+       lNeg = 1;
+       pStr++;
+     }
+     t_ptr = pStr;
+     t_length = strlen(t_ptr);
+     for(k=0;k < t_length;k++)
+     {
+       if (!isxdigit(*t_ptr))
+       {
+	 printf("Error: invalid hex operand, %s\n",orig_pStr);
+	 exit(4);
+       }
+       t_ptr++;
+     }
+     lNumLong = strtol(pStr, NULL, 16);    /* convert hex string into integer */
+     lNum = (lNumLong > INT_MAX)? INT_MAX : lNumLong;
+     if( lNeg )
+       lNum = -lNum;
+     return lNum;
+   }
+   else
+   {
+	printf( "Error: invalid operand, %s\n", orig_pStr);
+	exit(4);  /* This has been changed from error code 3 to error code 4, see clarification 12 */
+   }
+}
